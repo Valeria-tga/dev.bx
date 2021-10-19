7fd63cf3cb6f7efb65e224e545f990f72c3ab15b
valer <valeri.tga@gmail.com>
2021-10-19 16:22:03 +0200

    movies.php: Изменила php файл, в котором дан более полный список фильмов и здесь я произвожу обход массива.

--- movies.php
+++ movies.php
@@ -11,15 +11,169 @@ else{
 
 function film_by_age($age_user)
 {
-	$movies=["The Shawshank Redemption (1992), 16+. Rating - 9.3", "The Godfather (1972), 16+. Rating - 9.2",
-			 "The Dark Knight (2008), 14+. Rating - 9", "12 Angry Men (1957), 16+. Rating - 9", "Schindler's List (1993), 16+. Rating - 8.9"];
+	$movies = [
+		[
+			"title" => "The Shawshank Redemption",
+			"release_year" => 1994,
+			"duration" => 144,
+			"age_restriction" => 16,
+			"rating" => 9.3
+		],
+		[
+			"title" => "The Godfather",
+			"release_year" => 1972,
+			"duration" => 175,
+			"age_restriction" => 16,
+			"rating" => 9.2
+		],
+		[
+			"title" => "The Dark Knight",
+			"release_year" => 2008,
+			"duration" => 152,
+			"age_restriction" => 14,
+			"rating" => 9.0
+		],
+		[
+			"title" => "12 Angry Men",
+			"release_year" => 1957,
+			"duration" => 96,
+			"age_restriction" => 16,
+			"rating" => 9.0
+		],
+		[
+			"title" => "Schindler's List",
+			"release_year" => 1993,
+			"duration" => 195,
+			"age_restriction" => 16,
+			"rating" => 8.9
+		],
+		[
+			"title" => "The Lord of the Rings: The Return of the King",
+			"release_year" => 2003,
+			"duration" => 201,
+			"age_restriction" => 12,
+			"rating" => 8.9
+		],
+		[
+			"title" => "Pulp Fiction",
+			"release_year" => 1994,
+			"duration" => 154,
+			"age_restriction" => 18,
+			"rating" => 8.9
+		],
+		[
+			"title" => "Il buono, il brutto, il cattivo",
+			"release_year" => 1966,
+			"duration" => 161,
+			"age_restriction" => 12,
+			"rating" => 8.8
+		],
+		[
+			"title" => "Fight Club",
+			"release_year" => 1999,
+			"duration" => 139,
+			"age_restriction" => 18,
+			"rating" => 8.8
+		],
+		[
+			"title" => "Forrest Gump",
+			"release_year" => 1994,
+			"duration" => 142,
+			"age_restriction" => 0,
+			"rating" => 8.8
+		],
+		[
+			"title" => "Inception",
+			"release_year" => 2010,
+			"duration" => 148,
+			"age_restriction" => 12,
+			"rating" => 8.8
+		],
+		[
+			"title" => "The Matrix",
+			"release_year" => 1999,
+			"duration" => 136,
+			"age_restriction" => 16,
+			"rating" => 8.7
+		],
+		[
+			"title" => "Goodfellas",
+			"release_year" => 1990,
+			"duration" => 146,
+			"age_restriction" => 16,
+			"rating" => 8.7
+		],
+		[
+			"title" => "One Flew Over the Cuckoo's Nest",
+			"release_year" => 1975,
+			"duration" => 133,
+			"age_restriction" => 16,
+			"rating" => 8.7
+		],
+		[
+			"title" => "Shichinin no samurai",
+			"release_year" => 1954,
+			"duration" => 227,
+			"age_restriction" => 0,
+			"rating" => 8.6
+		],
+		[
+			"title" => "Se7en",
+			"release_year" => 1995,
+			"duration" => 127,
+			"age_restriction" => 18,
+			"rating" => 8.6
+		],
+		[
+			"title" => "The Silence of the Lambs",
+			"release_year" => 1991,
+			"duration" => 118,
+			"age_restriction" => 18,
+			"rating" => 8.6
+		],
+		[
+			"title" => "Cidade de Deus",
+			"release_year" => 2002,
+			"duration" => 130,
+			"age_restriction" => 16,
+			"rating" => 8.6
+		],
+		[
+			"title" => "La vita e bella",
+			"release_year" => 1997,
+			"duration" => 116,
+			"age_restriction" => 0,
+			"rating" => 8.6
+		],
+		[
+			"title" => "It's a Wonderful Life",
+			"release_year" => 1946,
+			"duration" => 130,
+			"age_restriction" => 0,
+			"rating" => 8.6
+		]
+	];
 
-	if (($age_user>=14) && ($age_user<16)){
-		echo  $movies[2];
+	$movies_len = count($movies);
+
+//foreach ($movies as $item){
+	//print_r($item["age_restriction"] ."\n");
+	//$i=1;
+//if ($age_user > $item["age_restriction"])
+//{
+	//echo $i . ". " . $item["title"] . " " . "(" .$item["release_year"] . ")" . ", " . " " . $item["age_restriction"] . "+. " . "Rating - " . $item["rating"] . "\n";
+//}
+	$number=1;
+for ($i=0; $i<$movies_len; $i++)
+{
+	if ($age_user > $movies[$i]["age_restriction"])
+	{
+		echo $number . ". " .  $movies[$i]["title"] . " " . "(" . $movies[$i]["release_year"] . ")" . ", " . " " .  $movies[$i]["age_restriction"] . "+. " . "Rating - " .  $movies[$i]["rating"] . "\n";
+		$number += 1;
 	}
-	elseif($age_user>=16){
-		foreach ($movies as $film)
-		{echo $film . "\n";}
-	}
+
+}
+
+
 }
 ?>
