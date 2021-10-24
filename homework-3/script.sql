21e83033693540db46a6136e904cf563c937a1c0
valer <valeri.tga@gmail.com>
2021-10-24 10:26:54 +0200

    script.sql: Добавление таблиц language и movie_title, заполнение их данными и удаление столбца TITLE из таблицы movie

---
+++ homework-3/script.sql
@@ -0,0 +1,36 @@
+CREATE TABLE language
+(
+	ID varchar(128) not null,
+	LANGUAGE_NAME varchar(500) not null,
+	PRIMARY KEY (ID)
+);
+
+CREATE TABLE movie_title
+(
+	MOVIE_ID int not null,
+	LANGUAGE_ID varchar(128) not null,
+	TITLE varchar(500) not null,
+
+	PRIMARY KEY(MOVIE_ID, LANGUAGE_ID),
+	FOREIGN KEY FK_MT_MOVIE(MOVIE_ID)
+		REFERENCES movie(ID)
+		ON UPDATE RESTRICT
+		ON DELETE RESTRICT,
+	FOREIGN KEY FK_MT_LANGUAGE(LANGUAGE_ID)
+		REFERENCES language(ID)
+		ON UPDATE RESTRICT
+		ON DELETE RESTRICT
+);
+
+INSERT INTO language (ID, LANGUAGE_NAME)
+VALUES ('ru', 'Русский язык'),
+       ('en', 'Английский язык'),
+       ('de', 'Немецкий язык');
+
+INSERT INTO movie_title (MOVIE_ID,LANGUAGE_ID, TITLE)
+SELECT ID,'ru', TITLE
+FROM movie;
+select * from movie_title
+
+ALTER TABLE movie
+	drop column TITLE;
